f95d348df33004644b506885de8310582cef5f8a
SkyTim <sky22357168@gmail.com>
2023-10-28 16:47:11 +0800

    containsDuplicate

--- 217.contains-duplicate.ts
+++ 217.contains-duplicate.ts
@@ -6,12 +6,15 @@
 
 // @lc code=start
 function containsDuplicate(nums: number[]): boolean {
+    /// Approach 1
     let set = new Set()
     for (let num of nums) {
         if (set.has(num)) return true
         set.add(num)
     }
     return false
+    /// Approach 2
+     return nums.length !== new Set(nums).size
 };
 
 /// test
