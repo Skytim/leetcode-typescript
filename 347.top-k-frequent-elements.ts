f2075e0d9ce377d19943d26b273896048a5f5b50
SkyTim <sky22357168@gmail.com>
2023-10-29 12:39:49 +0800

    Top K Frequent Elements

---
+++ 347.top-k-frequent-elements.ts
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=347 lang=typescript
+ *
+ * [347] Top K Frequent Elements
+ */
+
+// @lc code=start
+function topKFrequent(nums: number[], k: number): number[] {
+    const map: { [key: number]: number } = {};
+    nums.forEach(num => {
+        if (map[num] === undefined) {
+            map[num] = 1;
+        } else {
+            map[num]++;
+        }
+    })
+
+    return Object.entries(map).sort((a, b) => b[1] - a[1]).slice(0, k).map(item => Number(item[0]))
+};
+// @lc code=end
+
