09be1a3d192309a18279d2d14e432d51f8895938
SkyTim <sky22357168@gmail.com>
2023-11-05 00:03:33 +0800

    Demo init

--- ListNode.ts
+++
@@ -1,8 +0,0 @@
-declare class ListNode {
-    val: number
-    next: ListNode | null
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = (val === undefined ? 0 : val)
-        this.next = (next === undefined ? null : next)
-    }
-}
